2f83778f4fb2d8c5fe6ca880e18a1bd495f2476e
Misaka <chuxubank@qq.com>
2026-08-13 23:05:40 +0800

    feat: add icon to ansible delight and adjust formatting

--- modules/tools/+ansible.el
+++ modules/tools/+ansible.el
@@ -1,14 +1,14 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package ansible
-  :delight)
+  :delight (ansible-mode (:eval (+with-icon "nf-dev-ansible" " "))))
 
 (use-package ansible-doc
   :delight)
 
 (use-package poly-ansible-jinja2
   :vc (:url "https://github.com/cat-emacs/poly-any-template"
-       :lisp-dir "lisp/ansible"))
+            :lisp-dir "lisp/ansible"))
 
 (use-package flymake-ansible-lint
   :hook
