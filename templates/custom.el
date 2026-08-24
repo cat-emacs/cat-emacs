9fe7b592ba0056ac71ad32263aaf2d14d3801471
Misaka <chuxubank@qq.com>
2026-08-24 17:27:27 +0800

    refactor(logview): move shared formats to custom settings

--- templates/custom.el
+++ templates/custom.el
@@ -8,6 +8,52 @@
 ;;; Code:
 
 (custom-set-variables
+ '(logview-additional-timestamp-formats
+   '(("LogCat"
+      (java-pattern . "MM-dd HH:mm:ss.SSS"))
+     ("LogUtil"
+      (java-pattern . "HH:mm:ss.SSS"))
+     ("sing-box"
+      (java-pattern . "Z yyyy-MM-dd HH:mm:ss")))
+   t)
+ '(logview-additional-level-mappings
+   '(("LogCat"
+      (error "E" "F" "S")
+      (warning "W")
+      (information "I")
+      (debug "D")
+      (trace "V"))
+     ("Xray"
+      (error "Error")
+      (warning "Warning")
+      (information "Info")
+      (debug "Debug"))
+     ("Rclone"
+      (error "ERROR" "CRITICAL")
+      (warning "NOTICE")
+      (information "INFO")
+      (debug "DEBUG")))
+   t)
+ '(logview-additional-submodes
+   '(("LogCat"
+      (format . "TIMESTAMP IGNORED THREAD LEVEL NAME: MESSAGE")
+      (levels . "LogCat")
+      (timestamp "LogCat"))
+     ("LogUtil"
+      (format . "TIMESTAMP LEVEL/NAME [THREAD, IGNORED]: MESSAGE")
+      (levels . "LogCat")
+      (timestamp "LogUtil"))
+     ("Xray"
+      (format . "TIMESTAMP [LEVEL]<<RX:THREAD: \\[[^]]+\\] \\| >>NAME: MESSAGE")
+      (levels . "Xray"))
+     ("sing-box"
+      (format . "TIMESTAMP LEVEL <<RX:IGNORED:\\[\\|>><<RX:THREAD:[0-9]+\\|>><<RX:IGNORED: [^]]+\\]\\|>><<RX:NAME:[^:]+\\: \\|>>MESSAGE")
+      (levels . "SLF4J")
+      (timestamp "sing-box"))
+     ("Rclone"
+      (format . "TIMESTAMP LEVEL<<RX:IGNORED: *:>><<RX:THREAD:.+?: \\| >>MESSAGE")
+      (levels . "Rclone")))
+   t)
  '(gptel-model-updater-backends
    '(gptel--gemini gptel--llama gptel--mlx gptel--ollama
                    gptel--openrouter))
