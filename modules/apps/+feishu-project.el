26e2abd68c8fb9876c11b1663590b878fc001818
Misaka <chuxubank@qq.com>
2026-09-16 16:45:19 +0800

    fix(apps): remove unnecessary commands declaration for feishu-project

--- modules/apps/+feishu-project.el
+++ modules/apps/+feishu-project.el
@@ -8,9 +8,6 @@
 
 (use-package feishu-project
   :vc (:url "https://github.com/cat-emacs/feishu-project.el")
-  :commands
-  (feishu-project-list
-   feishu-project-mql)
   :custom
   (feishu-project-backend (if EMACS30+ 'mcp 'openapi)))
 
